41940a6b075050d71e6430996f4dcac76026a0e6
Yurii Ostapenko <yurii_ostapenko@epam.com>
2024-10-13 13:00:27 +0300

    Add about page

--- app1/main/views.py
+++ app1/main/views.py
@@ -4,15 +4,18 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        "title": "Home",
-        "content": "Main page of the shop",
-        "list": ["first", "second"],
-        "dict": {"first": 1},
-        "bool": True
+        "title": "Home - Главная",
+        "content": "Магазин мебели HOME",
+
     }
     return render(request, "main/index.html", context)
 
 
 def about(request):
-    return HttpResponse("About page")
+    context = {
+        "title": "Home - О нас",
+        "content": "О нас",
+        "text_on_page": "Текст о том почему этот магазин такой классный."
+    }
+    return render(request, "main/about.html", context)
 
